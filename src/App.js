3eec0a863c322c58b59b2cf5a67203842b49d6b6
Avivblanero <avivblanero@gmail.com>
2022-01-28 10:28:17 +0200

    the last static stage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Expenses from "./components/Expenses";
+
+
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'ביטוח רכב', amount: 4500, date: new Date(2021, 3, 8) },
+    { id: 'e2', title: 'נייר טואלט', amount: 45, date: new Date(2021, 2, 12) },
+    { id: 'e3', title: 'טלויזיה חדשה', amount: 2430, date: new Date(2021, 6, 5) },
+    { id: 'e4', title: 'ספרי לימוד', amount: 470, date: new Date(2021, 10, 12) },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <h2>מחשבון הוצאות</h2>
+      <Expenses item={expenses} />
+
     </div>
   );
 }
